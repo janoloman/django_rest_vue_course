172827199dd5a71fc4f9c909251c8a3479d8860d
Manuel Pedraza Gonzalez <manuelpedrazag@gmail.com>
2023-11-27 16:33:11 -0300

    minor changes

--- 02-WEB-APIs/04-Requests/02-requests.py
+++ 02-WEB-APIs/04-Requests/02-requests.py
@@ -2,11 +2,14 @@ import requests
 
 def main():
     # response = requests.get("https://api.exchangeratesapi.io/latest?base=USD&symbols=GBP")
-
+    url = "https://api.apilayer.com/exchangerates_data/latest"
+    headers = {
+        'apikey': '86OyucJHrWF5Mzg92fVawFmywCyl0KW6'
+    }
     payload = {"base": "USD", "symbols": "SEK"}
-    response = requests.get("https://api.exchangeratesapi.io/latest",
-                            params=payload)
-    
+    response = requests.get(url,
+                            headers=headers,params=payload)
+
     if response.status_code != 200:
         print("Status Code: ", response.status_code)
         raise Exception("There was an error!")
