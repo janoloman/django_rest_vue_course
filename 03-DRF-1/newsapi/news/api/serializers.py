1abdd4fdcd7799fa83e0c9334927114439772c4a
Manuel Pedraza Gonzalez <manuelpedrazag@gmail.com>
2023-12-05 13:16:52 -0300

    Nested relationships

--- 03-DRF-1/newsapi/news/api/serializers.py
+++ 03-DRF-1/newsapi/news/api/serializers.py
@@ -1,11 +1,15 @@
 from datetime import datetime
 from django.utils.timesince import timesince
 from rest_framework import serializers
-from news.models import Article
+from news.models import Article, Journalist
+
+
 
 class ArticleSerializer(serializers.ModelSerializer):
   
   time_since_publication = serializers.SerializerMethodField()
+  # author = JournalistSerializer(read_only=True)
+  # author = serializers.StringRelatedField()  
   
   class Meta:
     model = Article
@@ -29,8 +33,19 @@ class ArticleSerializer(serializers.ModelSerializer):
     if len(value) < 30:
       raise serializers.ValidationError("The Title has to be at least 60 characters long")
     return value
-    
-    
+
+
+class JournalistSerializer(serializers.ModelSerializer):
+  articles = serializers.HyperlinkedRelatedField(many=True,
+                                                 read_only=True,
+                                                 view_name="article-detail")
+  # articles = ArticleSerializer(many=True, read_only=True)
+
+  class Meta:
+    model = Journalist
+    fields= "__all__"
+
+
 
 # class ArticleSerializer(serializers.Serializer):
 #   id                = serializers.IntegerField(read_only=True)
