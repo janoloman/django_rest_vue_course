1a000bb35316f2f6f962c6a7684f3af1ad847e32
Manuel Pedraza Gonzalez <manuelpedrazag@gmail.com>
2023-12-12 12:33:34 -0300

    Permissions System part 1

--- 04-DRF-2/ebooksapi/ebooksapi/settings.py
+++ 04-DRF-2/ebooksapi/ebooksapi/settings.py
@@ -125,3 +125,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Rest framework config, default = AllowAny
+# REST_FRAMEWORK = {
+#     'DEFAULT_PERMISSION_CLASSES': [
+#         'rest_framework.permissions.IsAuthenticated',
+#     ]
+# }
