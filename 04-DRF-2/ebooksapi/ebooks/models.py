bd1d4304d8683ca82b68ea64878332d967492341
Manuel Pedraza González <manuelpedrazag@gmail.com>
2023-12-12 11:22:04 -0300

    ConcreteView classes

--- 04-DRF-2/ebooksapi/ebooks/models.py
+++ 04-DRF-2/ebooksapi/ebooks/models.py
@@ -12,15 +12,15 @@ class Ebook(models.Model):
     return self.title
 
 class Review(models.Model):
-  created_at = models.DateTimeField(auto_now_add=True)
-  updated_at = models.DateTimeField(auto_now=True)
+  created_at    = models.DateTimeField(auto_now_add=True)
+  updated_at    = models.DateTimeField(auto_now=True)
   review_author = models.CharField(max_length=8, blank=True, null=True)
-  review = models.TextField(blank=True, null=True)
-  rating = models.PositiveIntegerField(validators=[MinValueValidator(1),
-                                                   MaxValueValidator(5)])
-  ebook = models.ForeignKey(Ebook,
-                            on_delete=models.CASCADE,
-                            related_name="reviews")
+  review        = models.TextField(blank=True, null=True)
+  rating        = models.PositiveIntegerField(validators=[MinValueValidator(1),
+                                                          MaxValueValidator(5)])
+  ebook         = models.ForeignKey(Ebook,
+                                    on_delete=models.CASCADE,
+                                    related_name="reviews")
 
   def __str__(self):
     return str(self.rating)
