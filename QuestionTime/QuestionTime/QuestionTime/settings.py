ef249bb5e2eb15d71efc3c3ac3135f51ab470074
Manuel Pedraza Gonzalez <manuelpedrazag@gmail.com>
2023-12-28 17:55:38 -0300

    fix with django settings for login url

--- QuestionTime/QuestionTime/QuestionTime/settings.py
+++ QuestionTime/QuestionTime/QuestionTime/settings.py
@@ -140,7 +140,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Custom User Model
 AUTH_USER_MODEL = 'users.CustomUser'
 
-LOGIN_URL = "/accounts/login"
+LOGIN_URL = "/accounts/login/"
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
 
