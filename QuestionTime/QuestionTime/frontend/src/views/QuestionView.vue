4fdb10b12fb5f58407801851da84a478d5d0145a
Manuel Pedraza Gonzalez <manuelpedrazag@gmail.com>
2023-12-29 11:45:21 -0300

    project The Answer Component

--- QuestionTime/QuestionTime/frontend/src/views/QuestionView.vue
+++ QuestionTime/QuestionTime/frontend/src/views/QuestionView.vue
@@ -7,15 +7,36 @@
         <span class="author-name">{{ question.author }}</span>
       </p>
       <p>{{ question.created_at }}</p>
+      <hr>
     </div>
     <div v-else>
       <h1 class="error text-center">404 - Question not found</h1>
+    </div>
+
+    <div v-if="question">
+      <AnswerComponent
+        v-for="answer in answers"
+        :key="answer.uuid"
+        :answer="answer"
+      />
+    </div>
+
+    <div class="my-4">
+      <p v-show="loadingAnswers">...loading...</p>
+      <button
+        v-show="next"
+        @click="getQuestionAnswers"
+        class="btn btn-sm btn-outline-success"
+      >
+        Load more
+      </button>
     </div>
   </div>
 </template>
 
 <script>
 import { axios } from "@/common/api.service.js";
+import AnswerComponent from "@/components/Answer.vue";
 export default{
   name: "QuestionView",
   props: {
@@ -24,9 +45,15 @@ export default{
       required: true
     }
   },
+  components: {
+    AnswerComponent
+  },
   data() {
     return {
-      question: null
+      question: null,
+      answers: [],
+      next: null,
+      loadingAnswers: false,
     };
   },
   methods: {
@@ -44,10 +71,32 @@ export default{
         const title = "404 - not Found!";
         this.setPageTitle(title);
       }
-    }
+    },
+    async getQuestionAnswers() {
+      let endpoint = `/api/v1/questions/${this.slug}/answers/`;
+      if (this.next) {
+        endpoint = this.next;
+      }
+      this.loadingAnswers = true;
+      try {
+        const response = await axios.get(endpoint);
+        this.answers.push(...response.data.results);
+        console.log(this.answers);
+        this.loadingAnswers = false;
+        if (response.data.next) {
+          this.next = response.data.next;
+        } else {
+          this.next = null;
+        }
+      } catch (error) {
+        console.log(error.response);
+        alert(error.response.statusText);
+      }
+    },
   },
   created() {
     this.getQuestionData();
+    this.getQuestionAnswers();
   }
 }
 </script>
