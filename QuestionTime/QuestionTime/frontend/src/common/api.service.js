51e4efdaae9c22149fc755b11f69843c78d0ffc3
Manuel Pedraza Gonzalez <manuelpedrazag@gmail.com>
2023-12-29 12:47:24 -0300

    fix  "CSRF Failed: CSRF token missing." by sending an answer from frontend

--- QuestionTime/QuestionTime/frontend/src/common/api.service.js
+++ QuestionTime/QuestionTime/frontend/src/common/api.service.js
@@ -1,6 +1,17 @@
-const axios = require("axios");
+// const axios = require("axios");
+import axios from "axios";
+
+// Function to retrieve a cookie by name
+function getCookie(name) {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) return parts.pop().split(';').shift();
+}
 
 axios.default.xsrfCookieName = "xsrftoken";
 axios.default.xsrfHeaderName = "X-CSRFTOKEN";
+
+// Set CSRF token in headers
+axios.defaults.headers.common['X-CSRFToken'] = getCookie('csrftoken');
 
 export { axios };
